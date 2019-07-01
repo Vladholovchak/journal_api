b1c0cedce11d26f41fb7a31140cf7f57a5c16392
Vladholovchak <vlad.golovchak.1996@gmail.com>
2019-07-01 20:13:27 +0300

    Configure Rack Middleware, Install and configure devise, Create User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_202303) do
+ActiveRecord::Schema.define(version: 2019_06_26_204129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "groups", force: :cascade do |t|
     t.string "group_name", default: ""
-    t.date "start_stud_date", default: "2019-04-13"
+    t.date "start_stud_date"
     t.string "leader", default: ""
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -27,11 +27,23 @@ ActiveRecord::Schema.define(version: 2019_06_17_202303) do
     t.string "name", default: ""
     t.string "surname", default: ""
     t.string "father_name", default: ""
-    t.date "date_of_birth", default: "2019-04-13"
+    t.date "date_of_birth"
     t.bigint "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_students_on_group_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "students", "groups"
